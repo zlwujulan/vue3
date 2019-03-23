a853f4fa52ef9ae47ee98e808390d63e131082b3
liangliang <18336391418@163.com>
2019-03-23 21:05:40 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <div class="red">ndfgwe</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red{
+  font-size: 30px;
+  width:100%;
+  background: red;
+  height: 20px;
+}
+</style>
