36df1fd2ada46fead8a22ae75e749545869ad171
liangliang <18336391418@163.com>
2019-03-23 21:18:11 +0800

    提价

---
+++ src/components/head.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="head">
+   我是老大
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'head',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red{
+  font-size: 30px;
+  width:100%;
+ 
+}
+</style>
